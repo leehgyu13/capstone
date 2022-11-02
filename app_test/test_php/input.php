ad7a34202306a0c41e55865babfe821a3e3a762f
leehgyu13 <6398lhg@gmail.com>
2022-11-02 10:51:22 +0900

    input page

---
+++ app_test/test_php/input.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+    </head>   
+    <body>
+        <form action="insert.php" method="GET">
+            <p>ParkID : <input type="text" id="ParkID" name="ParkID"></p>
+            <p>CarID : <input type="text" id="CarID" name="CarID"></p>
+            <p>CarExist : <input type="text" id="CarExist" name="CarExist"></p>
+            <p>Chargestatus : <input type="text" id="Chargestatus" name="Chargestatus"></p>
+            <p><input type="submit" /></p>            
+        </form>
+    </body>
+</html>
