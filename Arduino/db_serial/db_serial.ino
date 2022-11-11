0e86a40ffb628b04f48f8202fdaf8e4276b4f543
leehgyu13 <6398lhg@gmail.com>
2022-11-11 16:21:56 +0900

    arduino to data base serial communication

--- Arduino/db_serial/db_serial.ino
+++ Arduino/db_serial/db_serial.ino
@@ -4,20 +4,10 @@ int ParkID;
 int CarID;
 int CarExist;
 int ChargeStatus;
+String ssid = "yeoi";
+String pwd = "qp98al76";
 
 SoftwareSerial esp01(2, 3);
-
-void setup() {
-  Serial.begin(9600);   //시리얼모니터
-  esp01.begin(9600); //와이파이 시리얼
-  delay(2000);
-  esp01.println("AT+RST\r\n");
-  delay(2000);
-  esp01.println("AT+CWMODE=3\r\n");
-  delay(2000);
-  esp01.println("AT+CWJAP=\"yeoi\",\"qp98al76\"\r\n");
-  delay(5000);
-}
 
 void printResponse() {
   while(esp01.available()){
@@ -25,25 +15,34 @@ void printResponse() {
   }
 }
 
-void loop() {
+void connectWifi() {
+  Serial.println("Connecting wifi...\n");
+  esp01.println("AT+CWJAP=\""+ssid+"\",\""+pwd+"\"\r\n");
+  delay(5000);
+  if(esp01.find("OK")) {
+    Serial.println("wifi connected\n");
+  } else {
+    Serial.println("Connect timeout\n");
+  }
+  //delay(1000);
+}
+
+void httpClient() {
   ParkID = 3;
   CarID = 6398;
   CarExist = 1;
   ChargeStatus = 1;
-  //Serial.println(ParkID);
-  //Serial.println(", ");
-  //Serial.println(CarID);
-  //Serial.println(", ");
-  //Serial.println(CarExist);
-  //Serial.println(", ");
-  //Serial.println(ChargeStatus);
-  //Serial.println("\n");
+  // 데이터 받는 코드 만들기
   esp01.println("AT+CIPMUX=1");
   delay(1000);
   printResponse();
+  Serial.println("Connecting TCP...\n");
   esp01.println("AT+CIPSTART=4,\"TCP\",\"leehgyu.iptime.org\",8080");
   delay(1000);
   printResponse();
+  if(Serial.find("ERROR")) return;
+  
+  Serial.println("Sending data...\n");
   String strParkID = String(ParkID);
   String strCarID = String(CarID);
   String strCarExist = String(CarExist);
@@ -56,6 +55,23 @@ void loop() {
   esp01.println();
   delay(200);
   printResponse();
+}
+
+void setup() {
+  Serial.begin(9600);   //시리얼모니터
+  esp01.begin(9600); //와이파이 시리얼
+  delay(2000);
+  esp01.println("AT+RST\r\n");
+  delay(2000);
+  esp01.println("AT+CWMODE=3\r\n");
+  delay(2000);
+  connectWifi();
+  //delay(500);
+}
+
+void loop() {
+  httpClient();
+  //delay(1000);
 
   exit(0);
   //delay(5000);
