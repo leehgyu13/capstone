fa12ee8c78f2605eb5fb77e5d0b612c209690719
leehgyu13 <6398lhg@gmail.com>
2022-11-14 20:03:02 +0900

    input page(GET to POST)

--- app_test/test_php/input.php
+++ app_test/test_php/input.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8"/>
     </head>   
     <body>
-        <form action="insert.php" method="GET">
+        <form action="insert.php" method="POST">
             <p>ParkID : <input type="text" id="ParkID" name="ParkID"></p>
             <p>CarID : <input type="text" id="CarID" name="CarID"></p>
             <p>CarExist : <input type="text" id="CarExist" name="CarExist"></p>
