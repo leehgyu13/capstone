c00d98857e5136ce193ac7681a9a9599d36eb752
leehgyu13 <6398lhg@gmail.com>
2022-11-15 00:49:19 +0900

    input page (if ParkID, CarID empty alert msg)

--- app_test/test_php/input.php
+++ app_test/test_php/input.php
@@ -4,9 +4,10 @@
         <meta charset="utf-8"/>
     </head>   
     <body>
-        <form action="insert.php" method="POST">
-            <p>ParkID : <input type="text" id="ParkID" name="ParkID" required></p>
-            <p>CarID : <input type="text" id="CarID" name="CarID" required></p>
+		<form action="insert.php" method="POST">
+		<p class="alert">* : 필수 입력 사항</p>
+		<p>ParkID : <input type="text" id="ParkID" name="ParkID" required><span class="alert"> * <?php echo $ParkIDMsg ?></span></p>
+		<p>CarID : <input type="text" id="CarID" name="CarID" required><span class="alert"> * <?php echo $CarIDMsg ?></span></p>
             <p>CarExist : <input type="text" id="CarExist" name="CarExist"></p>  //센서와 연동
             <p>Chargestatus : <input type="text" id="Chargestatus" name="Chargestatus"></p>
             <p><input type="submit" /></p>            
