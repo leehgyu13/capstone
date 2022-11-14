31364a7c9813eb5d7890d7402ab971fb75071072
leehgyu13 <6398lhg@gmail.com>
2022-11-15 00:38:32 +0900

    receive data from input and insert them to DB (check ParkID, CarID empty)

--- app_test/test_php/insert.php
+++ app_test/test_php/insert.php
@@ -10,8 +10,16 @@ $mysqli = new mysqli($db_host, $db_user, $db_passwd, $db_name);
     if($mysqli){
 	echo "MySQL successfully connected!<br/>";
 
-	$ParkID = $_POST['ParkID'];
-	$CarID = $_POST['CarID'];
+	if(empty($_POST['ParkID']){
+		$ParkIDMsg = "주차칸 번호를 입력하세요.";
+	} else {
+		$ParkID = $_POST['ParkID'];
+	}
+	if(empty($_POST['CarID']){
+		$CarIDMsg = "차량 번호를 입력하세요.";
+	} else {
+		$CarID = $_POST['CarID'];
+	}
     $CarExist = $_POST['CarExist'];
     $Chargestatus = $_POST['Chargestatus'];
 	
