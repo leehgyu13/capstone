c16b1c4b2d38192bc71268dd7de30bcfaf9fb29c
leehgyu13 <6398lhg@gmail.com>
2022-11-15 14:46:02 +0900

    receive data from input and insert them to DB (update)

--- app_test/test_php/insert.php
+++ app_test/test_php/insert.php
@@ -8,37 +8,28 @@ $db_name = "parking";
 $mysqli = new mysqli($db_host, $db_user, $db_passwd, $db_name);
  
     if($mysqli){
-	echo "MySQL successfully connected!<br/>";
+		echo "MySQL successfully connected!<br/>";
 
-	if(empty($_POST['ParkID']){
-		$ParkIDMsg = "주차칸 번호를 입력하세요.";
-	} else {
 		$ParkID = $_POST['ParkID'];
-	}
-	if(empty($_POST['CarID']){
 		$CarIDMsg = "차량 번호를 입력하세요.";
-	} else {
 		$CarID = $_POST['CarID'];
-	}
-    $CarExist = $_POST['CarExist'];
-    $Chargestatus = $_POST['Chargestatus'];
+    	$CarExist = $_POST['CarExist'];
+    	$Chargestatus = $_POST['Chargestatus'];
 	
-	echo "<br/>ParkID = $ParkID";
-	echo ", ";
-	echo "CarID = $CarID";
-    echo ", ";
-	echo "CarExist = $CarExist";
-    echo ", ";
-	echo "Chargestatus = $Chargestatus</br>";
-	
-	$query = "INSERT INTO parking (ParkID, CarID, CarExist, Chargestatus) VALUES ('$ParkID','$CarID', '$CarExist', '$Chargestatus')";
-	mysqli_query($mysqli,$query);
+		echo "<br/>ParkID = $ParkID";
+		echo ", ";
+		echo "CarID = $CarID";
+	    echo ", ";
+		echo "CarExist = $CarExist";
+	    echo ", ";
+		echo "Chargestatus = $Chargestatus</br>";
 
-	echo "</br>success!!";
-    }
+		$query = "UPDATE parking SET CarID = $CarID, CarExist = $CarExist, Chargestatus = $Chargestatus WHERE ParkID = '$ParkID'";
+		mysqli_query($mysqli,$query);
 
-    else{
-	echo "MySQL could not be connected";
+		echo "</br>success!!";
+    } else{
+		echo "MySQL could not be connected";
     }
 
 mysqli_close($mysqli);
