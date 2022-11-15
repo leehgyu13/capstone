c3d960534f0f08981f5bcf9e0d64e3b924e92219
leehgyu13 <6398lhg@gmail.com>
2022-11-15 18:09:42 +0900

    input page (POST to GET)

--- app_test/test_php/input.php
+++ app_test/test_php/input.php
@@ -4,9 +4,9 @@
         <meta charset="utf-8"/>
     </head>   
     <body>
-		<form action="insert.php" method="POST">
+		<form action="insert.php" method="GET">
 		<p class="alert">* : 필수 입력 사항</p>
-		<p>ParkID : <input type="text" id="ParkID" name="ParkID" required pattern="[0-3]+"><span class="alert"> * (0~3)</span></p>
+		<p>ParkID : <input type="text" id="ParkID" name="ParkID" required pattern="[0-2]+"><span class="alert"> * (0~2)</span></p>
 		<p>CarID : <input type="text" id="CarID" name="CarID" required><span class="alert"> * </span></p>
             <p>CarExist : <input type="text" id="CarExist" name="CarExist"></p>  <!-- 센서와 연동-->
             <p>Chargestatus : <input type="text" id="Chargestatus" name="Chargestatus"></p>
