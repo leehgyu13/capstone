d2fdec029a7336719a955b532ac636ba2dc50b44
leehgyu13 <6398lhg@gmail.com>
2022-11-15 14:44:15 +0900

    input page (ParkID range, ID required)

--- app_test/test_php/input.php
+++ app_test/test_php/input.php
@@ -6,9 +6,9 @@
     <body>
 		<form action="insert.php" method="POST">
 		<p class="alert">* : 필수 입력 사항</p>
-		<p>ParkID : <input type="text" id="ParkID" name="ParkID" required><span class="alert"> * <?php echo $ParkIDMsg ?></span></p>
-		<p>CarID : <input type="text" id="CarID" name="CarID" required><span class="alert"> * <?php echo $CarIDMsg ?></span></p>
-            <p>CarExist : <input type="text" id="CarExist" name="CarExist"></p>  //센서와 연동
+		<p>ParkID : <input type="text" id="ParkID" name="ParkID" required pattern="[0-3]+"><span class="alert"> * (0~3)</span></p>
+		<p>CarID : <input type="text" id="CarID" name="CarID" required><span class="alert"> * </span></p>
+            <p>CarExist : <input type="text" id="CarExist" name="CarExist"></p>  <!-- 센서와 연동-->
             <p>Chargestatus : <input type="text" id="Chargestatus" name="Chargestatus"></p>
             <p><input type="submit" /></p>            
         </form>
