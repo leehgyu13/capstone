2759137475811c854261f88a57b3dad92f63140e
leehgyu13 <6398lhg@gmail.com>
2022-11-23 23:28:12 +0900

    insert CarExist data to DB by ParkID (CarID, ChargeStatus removed)

--- app_test/test_php/insert.php
+++ app_test/test_php/insert.php
@@ -11,19 +11,12 @@ $mysqli = new mysqli($db_host, $db_user, $db_passwd, $db_name);
 		echo "MySQL successfully connected!<br/>";
 
 		$ParkID = $_GET['ParkID'];
-		$CarID = $_GET['CarID'];
     	$CarExist = $_GET['CarExist'];
-    	$Chargestatus = $_GET['Chargestatus'];
 	
 		echo "<br/>ParkID = $ParkID";
 		echo ", ";
-		echo "CarID = $CarID";
-	    echo ", ";
 		echo "CarExist = $CarExist";
-	    echo ", ";
-		echo "Chargestatus = $Chargestatus</br>";
-
-		$query = "UPDATE parking SET CarID = $CarID, CarExist = $CarExist, Chargestatus = $Chargestatus WHERE ParkID = '$ParkID'";
+		$query = "UPDATE parking SET CarExist = $CarExist WHERE ParkID = '$ParkID'";
 		mysqli_query($mysqli,$query);
 
 		echo "</br>success!!";
