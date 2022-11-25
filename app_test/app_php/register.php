499d34602a7a1eefeb75fe86654323a7affb4461
leehgyu13 <6398lhg@gmail.com>
2022-11-26 01:14:09 +0900

    register car only when car exist

--- app_test/app_php/register.php
+++ app_test/app_php/register.php
@@ -8,25 +8,19 @@ $db_name = "parking";
 $mysqli = new mysqli($db_host, $db_user, $db_passwd, $db_name);
  
     if($mysqli){
-		//echo "MySQL successfully connected!<br/>";
-
 		$ParkID = $_GET['ParkID'];
 		$CarID = $_GET['CarID'];
-    	//$CarExist = $_GET['CarExist'];
-    	//$Chargestatus = $_GET['Chargestatus'];
-	
-		//echo "<br/>ParkID = $ParkID";
-		//echo ", ";
-		//echo "CarID = $CarID";
-	    //echo ", ";
-		//echo "CarExist = $CarExist";
-	    //echo ", ";
-		//echo "Chargestatus = $Chargestatus</br>";
+		$query = "SELECT CarExist FROM `parking` WHERE ParkID = '$ParkID'";
+		$find = mysqli_query($mysqli,$query);
+		$data = mysqli_fetch_array($find);
+		$CarExist = (bool)$data['CarExist'];
 
-		$query = "UPDATE parking SET CarID = $CarID WHERE ParkID = '$ParkID'";
-		mysqli_query($mysqli,$query);
-
-		//echo "</br>success!!";
+		if($CarExist){
+			$query = "UPDATE parking SET CarID = $CarID WHERE ParkID = '$ParkID'";
+			mysqli_query($mysqli,$query);
+		} else {
+			echo "Please park first";
+		}
     } else{
 		echo "MySQL could not be connected";
     }
